74d14b428fff85ebee55f4406e4517ce3549a8c4
open17 <opensci@163.com>
2024-05-16 13:51:23 +0800

    feat: 初步起始页

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import SearchPage from './components/SearchPage.vue';
+import TopBar from './components/TopBar.vue';
+
+import {ref} from 'vue';
+
+let backgroundImg=ref('https://cdn.jsdelivr.net/gh/open17/Pic/img/202403011536167.jpeg')
+
+
+</script>
+
+<template>
+  <div class="flex w-screen h-screen flex-col bg-cover pt-20" :style="{'background-image':`url(${backgroundImg})`}">
+    <TopBar/>
+    <SearchPage/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/SearchPage.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex w-full h-full justify-start items-center flex-col">
+        <div class=" font-bold text-white text-8xl my-5 mb-14">
+            BiliVista
+        </div>
+        <input type="text" class=" w-3/6 h-12 rounded-3xl shadow-xl px-5 outline-none" placeholder="输入视频网址/bvid" />
+    </div>
+</template>
