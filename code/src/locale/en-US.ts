e41d8944bfedef95e653bfb323af6551692e7273
open17 <opensci@163.com>
2024-05-19 14:46:05 +0800

    feat: github链接

--- code/src/locale/en-US.ts
+++ code/src/locale/en-US.ts
@@ -19,6 +19,7 @@ export default {
   'menu.user': 'User Center',
   'menu.arcoWebsite': 'Arco Design',
   'menu.faq': 'FAQ',
+  'menu.github':"Github",
   'navbar.docs': 'Docs',
   'navbar.action.locale': 'Switch to English',
   ...localeSettings,
